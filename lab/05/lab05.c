1c11aad6fcdf360e6a8d5ee806cc6bbc82ce783d
Gkarios <kgareios13@gmail.com>
2024-11-25 18:07:13 +0200

    meme

---
+++ lab/05/lab05.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 100
+#define UNSEEN 0
+#define SEEN 1
+
+typedef struct
+{
+    int status;
+} Computer;
+
+int main()
+{
+    int NUM_COMPUTERS = N * N;
+    Computer computers[NUM_COMPUTERS + 1];
+    srand(time(NULL));
+
+    FILE *fp = fopen("lab04n=100.txt", "w");
+    if (fp == NULL)
+    {
+        perror("Unable to open file");
+        return 1;
+    }
+
+    for (int i = 1; i <= NUM_COMPUTERS; i++)
+    {
+        computers[i].status = UNSEEN;
+    }
+
+    int timestep = 0;
+    int first = (rand() % NUM_COMPUTERS) + 1;
+    computers[first].status = SEEN;
+    int informed = 1;
+    float α = (float)informed / NUM_COMPUTERS;
+    fprintf(fp, "%d %.4f\n", timestep, α);
+
+    if (informed < NUM_COMPUTERS)
+    {
+        double beta = 0.3;
+        while (informed < NUM_COMPUTERS)
+        {
+            if ((double)rand() / RAND_MAX < beta)
+            {
+                int random = (rand() % NUM_COMPUTERS) + 1;
+                if (computers[random].status == UNSEEN)
+                {
+                    computers[random].status = SEEN;
+                    informed++;
+                }
+            }
+            timestep++;
+            α = (float)informed / NUM_COMPUTERS;
+            fprintf(fp, "%d %.4f\n", timestep, α);
+        }
+    }
+    fclose(fp);
+    return 0;
+}
---
+++ lab/05/plotall.gp
@@ -0,0 +1,20 @@
+# Set up the output format and dimensions
+set terminal pngcairo size 1600, 1000
+set output 'coefficient.png'
+
+# Set up plot titles and labels
+set title "infection coefficient comparisson"
+set xlabel "Timestep"
+set ylabel "Informed sensors"
+set grid
+
+# Set the x-axis range
+set xrange [0:420000]
+
+# Plot the data from the three CSV files with distinct colors
+plot "lab04n=05.txt" using 1:2 with lines linecolor rgb "#2ca02c" title "N = 5", \
+"lab04n=10.txt" using 1:2 with lines linecolor rgb "#e64539" title "N = 10", \
+"lab04n=30.txt" using 1:2 with lines linecolor rgb "#1f77b4" title "N = 30", \
+"lab04n=50.txt" using 1:2 with lines linecolor rgb "#de996a" title "N = 50", \
+"lab04n=100.txt" using 1:2 with lines linecolor rgb "#e63998" title "N = 100", \
+
